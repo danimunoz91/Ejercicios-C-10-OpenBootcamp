fc4c24d6afb33fb16d37e85f4ce0fe26fb8978ff
Daniel Alejandro <daniel910509@gmail.com>
2022-09-07 10:37:54 -0500

    Exercise 2

---
+++ Ejercicio2_Hora/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite la hora:");
+int hora = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite los minutos:");
+int minutos = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite los segundos:");
+int segundos = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Hora " + hora + " Minutos: " + minutos + " Segundos: "+ segundos);
