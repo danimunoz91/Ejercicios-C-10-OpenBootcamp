5edc18cc87a0625ee4e04cc72c8fdae51086348b
Daniel Alejandro <daniel910509@gmail.com>
2022-09-07 14:33:39 -0500

    Ejercicio 4 condiciones

---
+++ Ejercicio5_Condiciones/Program.cs
@@ -0,0 +1,10 @@
+﻿int numero = 12;
+Console.WriteLine(numero > 18);
+
+char vocal = 'a';
+Console.WriteLine(vocal == 'a');
+
+int num1 = 5;
+int num2 = 7;
+Console.WriteLine("True and True: " + (num1 < 6 && num2  > 6));
+Console.WriteLine("True or False: " + (num1 < 6 || num2 < 6));
