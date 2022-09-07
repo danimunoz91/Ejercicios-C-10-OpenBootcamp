1fdff0597143ce93b2e965a9c1b5d07562066316
Daniel Alejandro <daniel910509@gmail.com>
2022-09-07 14:08:19 -0500

    Ejercicio 3 Datos persona

---
+++ Ejercicio3_DatosPerseona/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite nombre:"); 
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Digite apellido:");
+string apellido = Console.ReadLine();
+
+Console.WriteLine("Digite edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+string mensaje = nombre + ","+apellido + "," + edad + " sabe programar";
+
+Console.WriteLine(mensaje);
